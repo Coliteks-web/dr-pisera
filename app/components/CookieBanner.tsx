68a3e66506463c7aeb5a84b169e0cbe12b434b3a
Maciek <coliteks.web@gmail.com>
2025-07-18 16:06:15 +0200

    Dodanie Pikesla Meta

--- app/components/CookieBanner.tsx
+++ app/components/CookieBanner.tsx
@@ -19,40 +19,49 @@ export default function CookieBanner() {
     if (value === "granted") {
       loadAnalyticsScripts();
     }
-    setShowBanner(false); // ukryj baner po decyzji
+    setShowBanner(false); // Ukryj baner po decyzji
   };
 
   const loadAnalyticsScripts = () => {
     // Google Analytics
-    const script = document.createElement("script");
-    script.src = "https://www.googletagmanager.com/gtag/js?id=G-P2X5YKRSGJ";
-    script.async = true;
-    document.head.appendChild(script);
+    const gaScript = document.createElement("script");
+    gaScript.src = "https://www.googletagmanager.com/gtag/js?id=G-P2X5YKRSGJ";
+    gaScript.async = true;
+    document.head.appendChild(gaScript);
 
-    const inlineScript = document.createElement("script");
-    inlineScript.innerHTML = `
+    const gaInlineScript = document.createElement("script");
+    gaInlineScript.innerHTML = `
       window.dataLayer = window.dataLayer || [];
       function gtag(){dataLayer.push(arguments);}
       gtag('js', new Date());
       gtag('config', 'G-P2X5YKRSGJ');
     `;
-    document.head.appendChild(inlineScript);
+    document.head.appendChild(gaInlineScript);
 
     // Meta Pixel
     const fbScript = document.createElement("script");
     fbScript.innerHTML = `
       !function(f,b,e,v,n,t,s)
-      {if(f.fbq)return;n=f.fbq=function(){n.callMethod ?
-      n.callMethod.apply(n,arguments) : n.queue.push(arguments)};
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
       if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
       n.queue=[];t=b.createElement(e);t.async=!0;
       t.src=v;s=b.getElementsByTagName(e)[0];
       s.parentNode.insertBefore(t,s)}(window, document,'script',
       'https://connect.facebook.net/en_US/fbevents.js');
-      fbq('init', '123456789012345'); 
+      fbq('init', '1109474984377538');
       fbq('track', 'PageView');
     `;
     document.head.appendChild(fbScript);
+
+    // noscript pixel fallback
+    const noscriptImg = document.createElement("noscript");
+    noscriptImg.innerHTML = `
+      <img height="1" width="1" style="display:none"
+        src="https://www.facebook.com/tr?id=1109474984377538&ev=PageView&noscript=1"
+      />
+    `;
+    document.body.appendChild(noscriptImg);
   };
 
   if (!showBanner) return null;
