c85c9a7cb0c4f9d8e8b2b3a4ead413003334a055
Maciek <coliteks.web@gmail.com>
2026-05-31 19:32:36 +0200

    Dodanie głównej strony

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,9 +1,17 @@
+import HeroSection from '@/components/sections/HeroSection';
 import { getDictionary } from '@/i18n/getDictionary';
 import type { Locale } from '@/i18n';
-import HeroSection from '@/components/sections/HeroSection';
 
-export default async function Page() {
-  const dict = await getDictionary('pl');
+type PageProps = {
+  params: {
+    locale: Locale;
+  };
+};
+
+export default async function Page({ params }: PageProps) {
+  const { locale } = params;
+
+  const dict = await getDictionary(locale);
 
   return (
     <main>
