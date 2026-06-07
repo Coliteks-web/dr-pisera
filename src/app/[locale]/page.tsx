9d7a5265c2b6574633244c287e3299b7934ecc3f
Maciek <coliteks.web@gmail.com>
2026-06-07 18:38:21 +0200

    Zmiana strony

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -2,14 +2,12 @@ import HeroSection from '@/components/sections/HeroSection';
 import { getDictionary } from '@/i18n/getDictionary';
 import type { Locale } from '@/i18n';
 
-type PageProps = {
-  params: {
-    locale: Locale;
-  };
-};
-
-export default async function Page({ params }: PageProps) {
-  const { locale } = params;
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ locale: Locale }>;
+}) {
+  const { locale } = await params;
 
   const dict = await getDictionary(locale);
 
